604ce711c6c5bc6041d691e5b81219f0995e44ba
NithishBhat <nithishbhat1998@gmail.com>
2025-12-09 10:12:35 -0500

    final fix A5

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
   })
 );
 
